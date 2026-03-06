cd962e0c5e1692f2be14e848353ef323e1e506ea
Ger Doin <germandoin@gmail.com>
2026-03-05 21:51:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Cambia BrowserRouter por HashRouterimport Layout from "./components/Layout";
 import Index from "./pages/Index";
 import Guadalajara from "./pages/Guadalajara";
 import NotFound from "./pages/NotFound";
