bb46d6213a116c614afcac8424d324657722bb63
Ger Doin <germandoin@gmail.com>
2026-03-19 15:41:50 -0500

    Update Monterrey.tsx

--- src/pages/Monterrey.tsx
+++ src/pages/Monterrey.tsx
@@ -45,7 +45,7 @@ const FadeIn = ({
 const WHATSAPP_GDL =
   "https://api.whatsapp.com/send?phone=5218119972756&text=Hola%21+Me+interesa+conocer+m%C3%A1s+sobre+las+actividade+de+German+Doin+en+Monterrey.";
 const GOOGLE_FORM_CONFERENCIA =
-  "";
+  "https://api.whatsapp.com/send?phone=5218119972756&text=Hola%21+Me+interesa+conocer+m%C3%A1s+sobre+las+actividade+de+German+Doin+en+Monterrey.";
 
 const activities = [
   {
